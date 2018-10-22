f0099c13d1a37135c5efbfdc97be45d321034f35
nihar-mukhiya <niharmukhiya@gmail.com>
2018-10-22 22:44:25 +0530

    knn basic

--- nbc.py
+++ nbc.py
@@ -17,29 +17,38 @@ class_name = col_names[input1]
 uniq = e[col_names[input1]].unique()
 len_uniq = len(uniq)
 class_values = []
-
+numerical_class_values = []
 
 for y in range(len_uniq):
     clas1 = b.select(col_names[input1]).where(col_names[input1], uniq[y]).num_rows
     probs = clas1 / number_of_rows
     class_values.append(probs)
+    numerical_class_values.append(clas1)
 print(class_values)
 
 temp = copy.deepcopy(list(col_names))
 temp.remove(temp[input1])
-print(temp)
-dict = dict.fromkeys(uniq, [])
-
 print("enter constraints for each column: ")
 collect = []
+all_probs = []
 for i in range(len(temp)):
-    #list2 = []
+    list2 = []
     temp2 = input("for "+temp[i]+": ")
     collect.append(temp2)
     temp3 = b.where(temp[i], collect[i])
-    for y in range(len_uniq):
-        list2 = []
+    for y in range(0, len_uniq):
+        temp4 = 0
         temp4 = temp3.where(class_name, uniq[y]).num_rows
-        #list2.append(temp4)
-    dict[uniq[y]].append(temp4)
-print(dict)
+        temp4 = temp4 / numerical_class_values[y]
+        list2.append(temp4)
+    all_probs.append(list2)
+final_probs = []
+final_values = {}
+for k in range(0, len_uniq):
+    temp5 = 1
+    for j in range(0, len(all_probs)):
+        temp5 = temp5 * all_probs[j][k]
+    temp6 = temp5 * class_values[k]
+    final_values[uniq[k]] = temp6
+answer = max(final_values, key = final_values.get)
+print("According to your given constraints, the new value will belong to class: "+str(answer))
